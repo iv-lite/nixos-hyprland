bda85019e8bc8519abaa7c10aaf69a94b5bbed67
iv-lite <vibekwe@proton.me>
2024-11-03 06:03:52 +0100

    Fix - cage was white

--- services/greetd/default.nix
+++ services/greetd/default.nix
@@ -3,17 +3,6 @@
 , pkgs
 , ...
 }:
-let
-  load = pkgs.writeShellScript "load" ''
-    export XDG_SESSION_TYPE=wayland
-    export QT_QPA_PLATFORM=wayland
-    export XDG_SESSION_DESKTOP=Hyprland
-    export XDG_CURRENT_DESKTOP=Hyprland
-    export WLR_RENDERER=vulkan
-
-    cage -s -- regreet
-  '';
-in
 {
   environment.systemPackages = with pkgs; [
     cage
@@ -71,7 +60,7 @@ in
 
     settings = {
       default_session = {
-        command = "${load}";
+        command = "cage -s -- regreet --config /etc/greetd/regreet.toml";
         user = "greeter";
       };
     };
