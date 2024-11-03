113b4db469d78acd5be89039b77dca432953b742
iv-lite <vibekwe@proton.me>
2024-11-03 12:51:55 +0100

    Fix - unstable

--- hyprland/configuration.nix
+++ hyprland/configuration.nix
@@ -83,10 +83,10 @@
 
   # programs.hyprland.enable = true;
 
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    xwayland.enable = true;
-  };
+  # programs.hyprland = {
+  #   enable = true;
+  #   package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+  #   portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  #   xwayland.enable = true;
+  # };
 }
--- system/system-configuration.nix
+++ system/system-configuration.nix
@@ -29,63 +29,63 @@ in
   # hardware.i2c.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   # services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
+  # services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.acpilight.enable = true;
 
-  # hardware.graphics = {
-  #   enable = true;
-  #   enable32Bit = true;
-
-  #   extraPackages = with pkgs; [
-  #     amdvlk
-  #   ];
-  #   # For 32 bit applications 
-  #   extraPackages32 = with pkgs; [
-  #     driversi686Linux.amdvlk
-  #   ];
-  # };
-
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    # package = pkgs.unstable.mesa.drivers;
-    # package32 = pkgs.unstable.pkgsi686Linux.mesa.drivers;
-    # package = pkgs-hyprland.mesa.drivers;
-    # package32 = pkgs-hyprland.pkgsi686Linux.mesa.drivers;
-    driSupport = true;
-    driSupport32Bit = true;
-
-    # extraPackages = with pkgs.unstable; [
-    #   amdvlk
-    #   vulkan-loader
-    #   vulkan-validation-layers
-    #   vulkan-extension-layer
-    # ];
-
-    # extraPackages32 = with pkgs.unstable; [
-    #   driversi686Linux.amdvlk
-    # ];
-
+    enable32Bit = true;
 
     extraPackages = with pkgs; [
       amdvlk
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
     ];
-
+    #  For 32 bit applications 
     extraPackages32 = with pkgs; [
       driversi686Linux.amdvlk
     ];
-
-    # extraPackages = with pkgs-hyprland; [
-    #   amdvlk
-    # ];
-
-    # extraPackages32 = with pkgs-hyprland; [
-    #   driversi686Linux.amdvlk
-    # ];
   };
+
+  # hardware.opengl = {
+  #   enable = true;
+  #   # package = pkgs.unstable.mesa.drivers;
+  #   # package32 = pkgs.unstable.pkgsi686Linux.mesa.drivers;
+  #   # package = pkgs-hyprland.mesa.drivers;
+  #   # package32 = pkgs-hyprland.pkgsi686Linux.mesa.drivers;
+  #   driSupport = true;
+  #   driSupport32Bit = true;
+
+  #   # extraPackages = with pkgs.unstable; [
+  #   #   amdvlk
+  #   #   vulkan-loader
+  #   #   vulkan-validation-layers
+  #   #   vulkan-extension-layer
+  #   # ];
+
+  #   # extraPackages32 = with pkgs.unstable; [
+  #   #   driversi686Linux.amdvlk
+  #   # ];
+
+
+  #   extraPackages = with pkgs; [
+  #     amdvlk
+  #     vulkan-loader
+  #     vulkan-validation-layers
+  #     vulkan-extension-layer
+  #   ];
+
+  #   extraPackages32 = with pkgs; [
+  #     driversi686Linux.amdvlk
+  #   ];
+
+  #   # extraPackages = with pkgs-hyprland; [
+  #   #   amdvlk
+  #  # ];
+
+  #   # extraPackages32 = with pkgs-hyprland; [
+  #   #   driversi686Linux.amdvlk
+  #   # ];
+  # };
   # environment.variables.AMD_VULKAN_ICD = "vulkan";
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
