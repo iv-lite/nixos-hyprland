14b0eb9f5fe1bd9c779e92901f27c8a1f38e0963
iv-lite <vibekwe@proton.me>
2024-11-03 14:07:23 +0100

    Change - switched to stable channel

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
