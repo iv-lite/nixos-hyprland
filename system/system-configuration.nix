e6575c4ec333ba873ae6ddb2b93887545f5c5b6f
iv-lite <vibekwe@proton.me>
2024-11-16 04:35:38 +0100

    Fix - brightness

--- system/system-configuration.nix
+++ system/system-configuration.nix
@@ -33,6 +33,10 @@ in
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.acpilight.enable = true;
+  services.udev.extraRules = ''
+    KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
+  '';
+
 
   hardware.graphics = {
     enable = true;
