f2c499c9c67e2bf44aa1e444b5f8a11234e5709e
iv-lite <ibekwe.victor@gmail.com>
2025-01-12 11:09:06 +0100

    Fix - xwayland force-zero-scaling

--- softwares/default.nix
+++ softwares/default.nix
@@ -83,7 +83,7 @@
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    # xwayland.enable = true;
+    xwayland.enable = true;
   };
 
   xdg.portal = {
