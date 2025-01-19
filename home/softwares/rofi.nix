e82310fc54b0ad956ea16e27b3c9f701e73200cb
iv-lite <ibekwe.victor@gmail.com>
2025-01-19 04:27:00 +0100

    Fix - transparent

--- home/softwares/rofi.nix
+++ home/softwares/rofi.nix
@@ -1,39 +1,39 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
 
-    home.file = {
-        "${config.xdg.configHome}/rofi/themes" = {
-            source = ../../resources/.config/rofi/themes;
-            recursive = true;
-        };
+  home.file = {
+    "${config.xdg.configHome}/rofi/themes" = {
+      source = ../../resources/.config/rofi/themes;
+      recursive = true;
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    cycle = true;
+    terminal = "${pkgs.ghostty}/bin/ghostty";
+    plugins = [
+      pkgs.rofi-calc
+      pkgs.rofi-emoji
+      pkgs.rofi-systemd
+    ];
+
+    extraConfig = {
+      show-icons = true;
+      kb-cancel = "Escape,Super+space";
+      modi = "drun,run,combi,window,ssh,filebrowser,calc,emoji";
+      sort = true;
+
+      line-margin = 10;
+
+      display-ssh = "";
+      display-run = "";
+      display-drun = "";
+      display-window = "";
+      display-combi = "";
     };
 
-    programs.rofi = {
-        enable = true;
-        cycle = true;
-        terminal = "${pkgs.ghostty}/bin/ghostty";
-        plugins = [
-          pkgs.rofi-calc
-          pkgs.rofi-emoji
-          pkgs.rofi-systemd
-        ];
-
-        extraConfig = {
-          show-icons = true;
-          kb-cancel = "Escape,Super+space";
-          modi = "combi,window,run,drun,ssh,filebrowser,calc,emoji";
-          sort = true;
-
-          line-margin =10;
-
-          display-ssh =   "";
-          display-run =   "";
-          display-drun =  "";
-          display-window ="";
-          display-combi = "";
-        };
-
-        theme = "nord";
-    };
+    theme = "nord";
+  };
 }
