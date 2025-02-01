3e544627318f40ef9c7de2491963bca7a7c1a911
iv-lite <ibekwe.victor@gmail.com>
2025-02-01 17:57:57 +0100

    Fix - add override to nix-flatpak for wayland support

--- services/flatpak.nix
+++ services/flatpak.nix
@@ -13,5 +13,25 @@
         origin = "flathub";
       }
     ];
+    overrides = {
+      global = {
+        # Force Wayland by default
+        Context.sockets = [
+          "wayland"
+          "!x11"
+          "!fallback-x11"
+        ];
+
+        Environment = {
+          # Fix un-themed cursor in some Wayland apps
+          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+          # Force correct theme for some GTK apps
+          GTK_THEME = "Arc-Dark";
+
+          ELECTRON_OZONE_PLATFORM_HINT = "auto";
+        };
+      };
+    };
   };
 }
