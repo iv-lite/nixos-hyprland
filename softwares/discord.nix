59fc8f5848d6119d60cf50da4ec80f38e6027ed7
iv-lite <ibekwe.victor@gmail.com>
2025-02-02 16:47:43 +0100

    Fix - discord app using legcord

--- softwares/discord.nix
+++ softwares/discord.nix
@@ -7,13 +7,17 @@
   ...
 }:
 {
-  environment.systemPackages = with pkgs; [
-    (discord.override {
-      version = "0.0.82";
-      src = fetchurl {
-        url = "https://stable.dl2.discordapp.net/apps/linux/0.0.82/discord-0.0.82.tar.gz";
-        hash = "sha256-L8Lwe5UavmbW1s3gsSJiHjbiZnNtyEsEJzlrN0Fgc3w=";
-      };
-    })
+  # environment.systemPackages = with pkgs; [
+  #   (discord.override {
+  #     version = "0.0.82";
+  #     src = fetchurl {
+  #       url = "https://stable.dl2.discordapp.net/apps/linux/0.0.82/discord-0.0.82.tar.gz";
+  #       hash = "sha256-L8Lwe5UavmbW1s3gsSJiHjbiZnNtyEsEJzlrN0Fgc3w=";
+  #     };
+  #   })
+  # ];
+
+  environment.systemPackages = [
+    pkgs.legcord
   ];
 }
